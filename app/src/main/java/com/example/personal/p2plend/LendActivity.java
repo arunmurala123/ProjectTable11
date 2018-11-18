35a1876268e120a121b224d81a2c90fa02c0d69d
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2018-11-18 10:54:21 +0530

    adding lending activity xml and activity files

---
+++ app/src/main/java/com/example/personal/p2plend/LendActivity.java
@@ -0,0 +1,14 @@
+package com.example.personal.p2plend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lend);
+        setTitle("Enter the lend details");
+    }
+}
